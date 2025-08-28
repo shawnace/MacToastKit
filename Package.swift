b5cb0d07a1d6882b6e1a05ba23f98330d30d5202
Daniyal Master <daniyalmaster693@gmail.com>
2025-08-28 09:39:20 -0400

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -5,11 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "MacToastKit",
+    platforms: [
+        .macOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MacToastKit",
-            targets: ["MacToastKit"]),
+            targets: ["MacToastKit"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
